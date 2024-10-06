44aa03f17212895b3213f873b9a9147862930a9f
kshrugalj <kshrugalj@gmail.com>
2024-10-06 10:15:36 -0500

    Initial Commit

---
+++ Light APpTests/Light_APpTests.swift
@@ -0,0 +1,17 @@
+//
+//  Light_APpTests.swift
+//  Light APpTests
+//
+//  Created by Kshrugal Reddy Jangalapalli on 9/26/24.
+//
+
+import Testing
+@testable import Light_APp
+
+struct Light_APpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
